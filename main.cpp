96116cd6aaa2080cc5c7244a7cc1d8a8f8229aba
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2020-12-04 15:42:01 +0300

    Add LA_Viterbi test

--- main.cpp
+++ main.cpp
@@ -5,26 +5,5 @@
 
 
 int main() {
-    auto hmm = read_HMM("example.CHMM");
-    // Define sequence to examine
-    // GGCACTGAA
-    // A = 0, C = 1, G = 2, T = 3
-    auto seq = std::vector<HMM::Emit_t>{2,2,1,0,1,3,2,0,0};
-    // Expected result is
-    // (0,0) 25.6574
-    // (1,0) 24.4874
-    auto Viterbi_impl = LA_Viterbi();
-    Viterbi_impl.run_Viterbi(hmm, seq);
-    auto prob = HMM::Probability_t(0);
-    GrB_Matrix_extractElement_FP32(&prob, Viterbi_impl.result,  0, 0);
-    if (!HMM::almost_equal(prob, float(25.6574))) {
-        std::cerr << "Wrong (0,0) or result probabilities!\n"
-            << "(0,0) is " << prob << '\n';
-    }
-    GrB_Matrix_extractElement_FP32(&prob, Viterbi_impl.result, 1, 0);
-    if (!HMM::almost_equal(prob, float(24.4874))) {
-        std::cerr << "Wrong (0,1) or result probabilities!\n"
-            << "(1,0) is " << prob << '\n';
-    }
     return 0;
 }
---
+++ tests/test_LA_Viterbi.cpp
@@ -0,0 +1,27 @@
+#include "../LA_Viterbi.h"
+#include "../HMM_reader.h"
+
+#include <iostream>
+int main() {
+    auto hmm = read_HMM("../CHMM_data/example.CHMM");
+    // Define sequence to examine
+    // GGCACTGAA
+    // A = 0, C = 1, G = 2, T = 3
+    auto seq = std::vector<HMM::Emit_t>{2,2,1,0,1,3,2,0,0};
+
+    auto Viterbi_impl = LA_Viterbi();
+    Viterbi_impl.run_Viterbi(hmm, seq);
+
+    // Expected result is
+    // (0,0) 25.6574
+    // (1,0) 24.4874
+    auto prob_0 = HMM::Probability_t(0);
+    auto prob_1 = HMM::Probability_t(0);
+    GrB_Matrix_extractElement_FP32(&prob_0, Viterbi_impl.result, 0, 0);
+    GrB_Matrix_extractElement_FP32(&prob_1, Viterbi_impl.result, 1, 0);
+    auto is_test_passed =
+        HMM::almost_equal(prob_0, HMM::Probability_t(25.6574)) &&
+        HMM::almost_equal(prob_1, HMM::Probability_t(24.4874));
+    std::cerr << prob_0 << ' ' << prob_1 << '\n';
+    return 1 - static_cast<int>(is_test_passed);
+}
