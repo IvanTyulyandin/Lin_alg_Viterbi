7790e8fdfe506df17115582257134d7918e3474a
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2020-12-08 14:37:52 +0300

    Add .chmm generator

--- ess_files/ess_gen.py
+++ ess_files/ess_gen.py
@@ -3,9 +3,9 @@ import random
 # Parameters
 how_much_to_gen: int = 3
 seq_size: int = 3500
-allowed_range: int = 20
+emit_range: int = 20
 file_name: str = "emit_" + str(how_much_to_gen) + \
-    "_" + str(seq_size) + "_" + str(allowed_range) + ".ess"
+    "_" + str(seq_size) + "_" + str(emit_range) + ".ess"
 len_per_line: int = 100
 
 # Generation
@@ -14,7 +14,7 @@ with open(file_name, 'w') as f:
         f.write(str(i) + ' ' + str(seq_size) + '\n')
         random_seq: list = []
         for _ in range(seq_size):
-            random_seq.append(str(random.randrange(allowed_range)))
+            random_seq.append(str(random.randrange(emit_range)))
         res_seq: list = [" ".join(random_seq[i:i + len_per_line]) + '\n'
                          for i in range(0, seq_size, len_per_line)]
         f.writelines(res_seq)
