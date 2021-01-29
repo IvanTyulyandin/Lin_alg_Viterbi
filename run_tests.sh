ece9ed3dcad44c0cc59b6f50da4a1919c37dafbd
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2021-01-29 22:01:48 +0300

    Add option -v to run tests with valgrind

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 cd cmake_build
-ctest --verbose -D ExperimentalMemCheck .
+case "$1" in
+    -v)
+        ctest --verbose -D ExperimentalMemCheck .
+        exit 0
+        ;;
+    *) ctest .
+        exit 0
+        ;;
+esac
 cd ..
